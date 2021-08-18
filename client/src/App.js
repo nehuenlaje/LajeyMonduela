6523994bc0f297c6eea7699af1e963be868e784b
44-2-kupa-martin <kupamartinclassroom@gmail.com>
2021-08-18 17:51:32 -0300

    agrega front

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
